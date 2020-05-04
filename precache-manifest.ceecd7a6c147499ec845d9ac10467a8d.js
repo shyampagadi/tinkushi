cdeda6ab3da0949add9cf33141e10d1be0eefd4c
shyampagadi <shyam.pagadi@gmail.com>
2020-05-04 13:59:26 +0530

    Updates

---
+++ precache-manifest.ceecd7a6c147499ec845d9ac10467a8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f153944700ca64f0e1c4e5e49ea705",
+    "url": "/shyampagadi/tinkushi/index.html"
+  },
+  {
+    "revision": "9c02c8ed0e713a16f25e",
+    "url": "/shyampagadi/tinkushi/static/css/main.99ab8ab5.chunk.css"
+  },
+  {
+    "revision": "208dacc7e239814d330a",
+    "url": "/shyampagadi/tinkushi/static/js/2.50e2063b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shyampagadi/tinkushi/static/js/2.50e2063b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c02c8ed0e713a16f25e",
+    "url": "/shyampagadi/tinkushi/static/js/main.689ab2a5.chunk.js"
+  },
+  {
+    "revision": "863bd2a565109e604a85",
+    "url": "/shyampagadi/tinkushi/static/js/runtime-main.1fb1e31e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shyampagadi/tinkushi/static/media/logo.5d5d9eef.svg"
+  }
+]);
